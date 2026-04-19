c5e53fb7aaf388e3f5c7e3d452d301b9401b6256
Kateryna212 <koretcka.kate@e-u.edu.ua>
2026-04-19 17:02:59 +0300

    Implement modular file listing functionality

---
+++ make-it-modular.js
@@ -0,0 +1,8 @@
+const mymodule = require('./mymodule.js');
+const dir = process.argv[2];
+const ext = process.argv[3];
+
+mymodule(dir, ext, function (err, list) {
+  if (err) return console.error(err);
+  list.forEach(file => console.log(file));
+});
