63dc95c602fb87f720ecc27a7b91ff00764a2a0e
Kateryna212 <koretcka.kate@e-u.edu.ua>
2026-04-19 17:04:09 +0300

    Add http-collect.js to fetch and display HTTP data

---
+++ http-collect.js
@@ -0,0 +1,10 @@
+const http = require('http');
+http.get(process.argv[2], function (response) {
+  let result = '';
+  response.setEncoding('utf8');
+  response.on('data', (data) => result += data);
+  response.on('end', () => {
+    console.log(result.length);
+    console.log(result);
+  });
+});
