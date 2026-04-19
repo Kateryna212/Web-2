bc2257917b447dde9d2e788e1bd9d357d9519b4e
Kateryna212 <koretcka.kate@e-u.edu.ua>
2026-04-19 17:09:21 +0300

    Implement HTTP server to uppercase POST data

---
+++ http-uppercaserer.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const map = require('through2-map');
+
+const server = http.createServer(function (req, res) {
+  if (req.method !== 'POST') return res.end('Send a POST\n');
+  req.pipe(map(chunk => chunk.toString().toUpperCase())).pipe(res);
+});
+server.listen(Number(process.argv[2]));
